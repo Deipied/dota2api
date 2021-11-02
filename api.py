ce34fda3e3b8af2946bd976f33c1585882278efe
Hoon Cho <hoonhyungcho@gmail.com>
2021-11-02 11:03:11 -0500

    working on and testing dota2api

---
+++ api.py
@@ -0,0 +1,19 @@
+import requests, json, time, os
+
+# downloadmatch dta from open dota
+def get_match_by_id(match_id):
+    m_id = str(match_id)
+    # fetch match data
+    request = requests.get("https://api.opendota.com/api/matches/" + m_id)
+    if request.ok:
+        print("GET:", m_id)
+        data = request.json()
+        # save it
+        file = open("download" + os.pathsep + m_id + '_data.json', 'w')
+        json.dump(data.file)
+        file.close()
+
+match_id = 5820544839
+for i in range(0,10):
+    get_match_by_id(match_id + i)
+    time.sleep(2)
